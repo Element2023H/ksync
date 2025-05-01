dcd48685a325b70b1fb67be8ed288eb858dfbcde
jacky <lzty.jw@gmail.com>
2025-05-01 18:59:01 +0800

    implement associate drop method for LazyOnce<T>

--- src/lazy.rs
+++ src/lazy.rs
@@ -2,6 +2,7 @@ use core::{
     cell::UnsafeCell,
     mem::ManuallyDrop,
     ops::Deref,
+    ptr::drop_in_place,
     sync::atomic::{self, AtomicU32},
 };
 
@@ -30,6 +31,45 @@ impl<T, F: FnOnce() -> T> LazyOnce<T, F> {
                 f: ManuallyDrop::new(f),
             }),
         }
+    }
+
+    /// # Description
+    /// use this method to drop `T` inside a `LazyOnce`</br>
+    /// `LazyOnce` will not be automatically dropped in kernel programming since kernel leaks something like CRT runtime code</br>
+    /// NOR do we can use the `into_inner()` semantics here since rust forbidden move out of static `LazyOnce`
+    ///
+    /// # Safety
+    /// - the caller must call this method at most once
+    /// - access the wrapped `T` after dropped can cause undefined behavior
+    /// - call `drop()` more than once can cause undefined behavior
+    /// - use this method only for the global static initialized `LazyOnce`
+    ///
+    /// # Examples
+    /// ```
+    /// // declares LazyOnce in somewhere
+    /// static GLOBAL_INSTANCE: LazyOnce<u32> = LazyOnce::new(|| 0);
+    ///
+    /// void driver_unload(driver_object: PDRIVER_OBJECT) {
+    ///     // the inner T will be dropped here
+    ///     LazyOnce::drop(&GLOBAL_INSTANCE);
+    ///     // ... do some other stuff
+    /// }
+    /// ```
+    pub fn drop(this: &LazyOnce<T, F>) {
+        const UINIT: u32 = State::Uninit as u32;
+        const INITIALIZED: u32 = State::Initialized as u32;
+
+        let state = this.state.load(atomic::Ordering::Relaxed);
+
+        let data = unsafe { &mut *this.data.get() };
+
+        match state {
+            UINIT => unsafe { ManuallyDrop::drop(&mut data.f) },
+            INITIALIZED => unsafe { ManuallyDrop::drop(&mut data.value) },
+            _ => panic!(),
+        }
+
+        unsafe { drop_in_place(this.state.as_ptr()) };
     }
 
     pub fn force(this: &LazyOnce<T, F>) -> &T {
@@ -93,9 +133,8 @@ impl<T, F: FnOnce() -> T> Deref for LazyOnce<T, F> {
     }
 }
 
-const UNINIT: u32 = State::Uninit as u32;
-const INITIALIZED: u32 = State::Initialized as u32;
-
+// `lazyOnce` can be used in many cases not only the global static one
+// provide this method for compatible with RAII in rust
 impl<T, F> Drop for LazyOnce<T, F> {
     fn drop(&mut self) {
         match *self.state.get_mut() {
@@ -114,9 +153,9 @@ impl<T, F> Drop for LazyOnce<T, F> {
 // we DO NOT constraint the `T` with `Sync + Send` because some native kernel structs
 // contains raw pointers which neither be marked as `Sync` or `Send` by default and thus can not be
 // wrapped in LazyOnce which whill fail the compile-time checking
-// 
+//
 // Note
 // it is the caller's responsibility to ensure that the raw pointers in `T` can be access safely among multi-thread
 //
 // unsafe impl<T: Sync + Send, F: Send> Sync for LazyOnce<T, F> {}
-unsafe impl<T, F: Send> Sync for LazyOnce<T, F>{}
+unsafe impl<T, F: Send> Sync for LazyOnce<T, F> {}
