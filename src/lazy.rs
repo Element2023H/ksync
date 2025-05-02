4aa43b84d81ccb8d8cee33108c915992a7cc7f1c
jacky <lzty.jw@gmail.com>
2025-05-02 10:59:49 +0800

    optimize lazy module

--- src/lazy.rs
+++ src/lazy.rs
@@ -1,32 +1,61 @@
 use core::{
     cell::UnsafeCell,
-    mem::ManuallyDrop,
+    mem::{self, ManuallyDrop},
     ops::Deref,
     ptr::drop_in_place,
     sync::atomic::{self, AtomicU32},
 };
-
-enum State {
-    Uninit,
-    Initializing,
-    Initialized,
-}
 
 union Data<T, F> {
     value: ManuallyDrop<T>,
     f: ManuallyDrop<F>,
 }
 
+/// `LazyOnce` is used to initialize `T` exclusively
+/// 
+/// # Safety
+/// - ensure only one thread can only initialize `T` once, other thread must wait until the initializing completed
+/// thus no data race occurred during initializing progress
+/// - ensure only shared refs can be gained, so it can be shared between multi-threads
+/// 
+/// # Example
+/// ```
+/// // typical usage
+/// // declare a `LazyOnce` somewhere
+/// static GLOBAL_INSTANCE: lazyONce<0u32> = LazyOnce::new(|| 0);
+/// 
+/// fn use_global_instance() {
+///     println!("value = {}", *GLOBAL_INSTANCE);
+/// }
+/// 
+/// // destroy instance in DriverUnload
+/// fn driver_unload(driver_object: PDRIVER_OBJECT) {
+///     // the caller must ensure NOT use it again after it dropped
+///     lazyOnce::drop(&GLOBAL_INSTANCE);
+/// }
+/// ```
+/// 
+/// # Note
+/// since only shared refs can be gained through a `LazyOnce`,
+/// so if one want to changed the value of `T` inside a `LazyOnce` concurrently,
+/// consider wrap `T` within a `Locked` instead.
+/// 
+/// see `Locked<T>` for details
+/// ```
 pub struct LazyOnce<T, F = fn() -> T> {
     state: AtomicU32,
 
     data: UnsafeCell<Data<T, F>>,
 }
 
+const UNINIT: u32 = 0;
+const INITIALIZING: u32 = 1;
+const INITIALIZED: u32 = 2;
+
 impl<T, F: FnOnce() -> T> LazyOnce<T, F> {
     pub const fn new(f: F) -> Self {
         Self {
-            state: AtomicU32::new(State::Uninit as u32),
+            state: AtomicU32::new(UNINIT),
             data: UnsafeCell::new(Data {
                 f: ManuallyDrop::new(f),
             }),
@@ -56,71 +85,73 @@ impl<T, F: FnOnce() -> T> LazyOnce<T, F> {
     /// }
     /// ```
     pub fn drop(this: &LazyOnce<T, F>) {
-        const UINIT: u32 = State::Uninit as u32;
-        const INITIALIZED: u32 = State::Initialized as u32;
-
         let state = this.state.load(atomic::Ordering::Relaxed);
 
         let data = unsafe { &mut *this.data.get() };
 
         match state {
-            UINIT => unsafe { ManuallyDrop::drop(&mut data.f) },
+            UNINIT => unsafe { ManuallyDrop::drop(&mut data.f) },
             INITIALIZED => unsafe { ManuallyDrop::drop(&mut data.value) },
             _ => panic!(),
         }
 
+        // Safety
+        // we must ensure all the members be dropped in manually drop operation to prevent memory leaks
         unsafe { drop_in_place(this.state.as_ptr()) };
     }
 
     pub fn force(this: &LazyOnce<T, F>) -> &T {
         let state = this.state.load(atomic::Ordering::Relaxed);
 
-        if state == State::Initialized as u32 {
-            return unsafe { &(*this.data.get()).value };
+        match state {
+            UNINIT => LazyOnce::really_init(this),
+            INITIALIZING => this.force_wait(),
+            INITIALIZED => unsafe { &(*this.data.get()).value },
+            _ => panic!("invalid state value")
         }
+    }
 
-        if state == State::Initializing as u32 {
-            this.wait();
-            return unsafe { &(*this.data.get()).value };
-        } else {
-            if let Ok(old_value) = this.state.compare_exchange(
-                State::Uninit as _,
-                State::Initializing as _,
-                atomic::Ordering::SeqCst,
-                atomic::Ordering::Relaxed,
-            ) {
-                assert_eq!(old_value, State::Uninit as _);
+    fn really_init(this: &LazyOnce<T, F>) -> &T {
+        if let Ok(old_value) = this.state.compare_exchange(
+            UNINIT,
+            INITIALIZING,
+            atomic::Ordering::SeqCst,
+            atomic::Ordering::Relaxed,
+        ) {
+            unsafe {
+                let data = &mut (*this.data.get());
 
-                unsafe {
-                    let data = &mut (*this.data.get());
+                let f = ManuallyDrop::take(&mut data.f);
 
-                    let f = ManuallyDrop::take(&mut data.f);
+                let value = f();
 
-                    let value = f();
+                (*this.data.get()).value = ManuallyDrop::new(value);
 
-                    (*this.data.get()).value = ManuallyDrop::new(value);
+                let _ = this.state.compare_exchange(
+                    INITIALIZING,
+                    INITIALIZED,
+                    atomic::Ordering::Release,
+                    atomic::Ordering::Relaxed,
+                );
 
-                    let _ = this.state.compare_exchange(
-                        State::Initializing as _,
-                        State::Initialized as _,
-                        atomic::Ordering::Release,
-                        atomic::Ordering::Relaxed,
-                    );
-
-                    return &(*this.data.get()).value;
-                }
-            } else {
-                this.wait();
-                return unsafe { &(*this.data.get()).value };
+                &(*this.data.get()).value
             }
+        } else {
+            this.force_wait()
         }
+    }
+
+    fn force_wait(&self) -> &T {
+        self.wait();
+
+        unsafe { &(*self.data.get()).value }   
     }
 
     // wait until the state becomes State::Initialized
     fn wait(&self) {
         use core::arch::x86_64::_mm_pause;
 
-        while self.state.load(atomic::Ordering::Acquire) != State::Initialized as u32 {
+        while self.state.load(atomic::Ordering::Acquire) != INITIALIZED {
             unsafe { _mm_pause() };
         }
     }
@@ -159,3 +190,115 @@ impl<T, F> Drop for LazyOnce<T, F> {
 //
 // unsafe impl<T: Sync + Send, F: Send> Sync for LazyOnce<T, F> {}
 unsafe impl<T, F: Send> Sync for LazyOnce<T, F> {}
+
+enum State<T, F> {
+    Uninit(F),
+    Init(T),
+    Poisoned,
+}
+
+/// # Synopsis
+/// it is used to initialize a `static const` variable that must be initialized at most once but can be used safely in multi-thread</br>
+/// this class behave mostly like `LazyOnce` but the users can not get a mutable ref from a `LazyStatic`
+/// 
+/// # fetures
+/// - naturally no data race during initialization(caller must follow the safety rules below)
+/// - a `LazyStatic` is memory efficient than a `lazyOnce`, it only require size_of(T) size in memory storage
+/// 
+/// ## Safety
+/// - caller must ensure it to be initialized only once, for example: intialize it in DriverEntry by calling `LazyStatic::force`
+/// - can be shared between multi-threads
+/// - can not obtain a mutable reference through a `LazyStatic`
+/// - no interior mutability once it has been initialized
+/// 
+/// # Note
+/// - `LazyStatic` does not allocate memory in kernel heap, consider using `Box<T>` if `T` must be allocated dynamically
+/// - if the caller want to read-write the wrapped `T` concurrently, consider wrap `T` into a `Locked<T>`
+/// - once a `LazyStatic` is initialized in static contex(typically a static instance), it is in pinned memory
+/// 
+/// # Example
+/// ```
+/// type FnAPI = extern "system" fn ();
+/// 
+/// pub static KERNEL_API: LazyStatic<Option<FnAPI>> = LazyStatic::new(|| get_kernel_api());
+/// 
+/// // use it somewhere as follows:
+/// fn driver_entry(...) {
+///     if let Some(func) = *KERNEL_API {
+///         // ...
+///     }
+/// }
+/// ```
+pub struct LazyStatic<T, F = fn() -> T> {
+    state: UnsafeCell<State<T, F>>,
+}
+
+impl<T, F: FnOnce() -> T> LazyStatic<T, F> {
+    pub const fn new(f: F) -> Self {
+        Self {
+            state: UnsafeCell::new(State::Uninit(f)),
+        }
+    }
+
+    pub fn into_inner(this: LazyStatic<T, F>) -> Result<T, F> {
+        match this.state.into_inner() {
+            State::Init(data) => Ok(data),
+            State::Uninit(f) => Err(f),
+            State::Poisoned => panic!("LazyStatic is not initialized")
+        }
+    }
+
+    pub fn get(&self) -> Option<&T> {
+        let state = unsafe { &*self.state.get() };
+
+        match state {
+            State::Init(data) => Some(data),
+            _ => None
+        }
+    }
+
+    /// Safety:
+    /// 
+    /// it is the caller's responsibility to call force() in signle-thread mode to avoid data race
+    pub fn force(this: &LazyStatic<T, F>) -> &T {
+        let state = unsafe { &*this.state.get() };
+
+        match state {
+            State::Init(data) => data,
+            State::Uninit(_) => unsafe { LazyStatic::really_init(this) },
+            State::Poisoned => {
+                panic!("LazyStatic is in poisoned state, maybe it has been used incorrectly")
+            }
+        }
+    }
+
+    unsafe fn really_init(this: &LazyStatic<T, F>) -> &T {
+        let state = unsafe { &mut *this.state.get() };
+
+        let State::Uninit(f) = mem::replace(state, State::Poisoned) else {
+            unreachable!()
+        };
+
+        let data = f();
+
+        unsafe { this.state.get().write(State::Init(data)) };
+
+        let state = unsafe { &*this.state.get() };
+
+        let State::Init(data) = state else {
+            unreachable!()
+        };
+
+        data
+    }
+}
+
+impl<T, F: FnOnce() -> T> Deref for LazyStatic<T, F> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        LazyStatic::force(self)
+    }
+}
+
+// about why we do not constraint `Sync + Send` agiant `T`, see ksync::lazy::LazyOnce for details
+unsafe impl<T, F: FnOnce() -> T> Sync for LazyStatic<T, F> {}
