1bd64ba9516d42ae7981db012c443947032fbc92
jacky <lzty.jw@gmail.com>
2025-05-14 22:27:54 +0800

    update to 0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,20 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 
-pub mod utils;
-pub mod mutex;
-pub mod once;
-pub mod handle;
 pub mod dpc;
 pub mod event;
-pub mod timer;
+pub mod handle;
 pub mod kobject;
-pub mod semaphore;
 pub mod lazy;
+pub mod mutex;
 pub mod ntstatus;
+pub mod once;
+pub mod semaphore;
 pub mod thread;
+pub mod timer;
+pub mod utils;
 
-#[deprecated(since="0.1.1", note="please use `mutex` instead")]
+#[deprecated(since = "0.1.1", note = "please use `mutex` instead")]
 pub mod lock;
 
 mod constants;
@@ -28,15 +28,22 @@ extern crate alloc;
 
 // call it in another driver to do the testing
 pub mod test {
+    use core::ptr;
     use core::time::Duration;
 
     use alloc::sync::Arc;
     use wdk::println;
+    use wdk_sys::ntddk::KeGetCurrentProcessorNumberEx;
 
     use crate::lock;
 
     use super::mutex::*;
     use super::thread::*;
+    use super::event::*;
+    use super::dpc::{self};
+    use super::semaphore::*;
+    use super::kobject::*;
+    use super::timer::*;
 
     extern crate alloc;
 
@@ -84,7 +91,10 @@ pub mod test {
         }
 
         // check the shared counter
-        println!("the final value of shared counter is: {:?}", **shared_counter);
+        println!(
+            "the final value of shared counter is: {:?}",
+            **shared_counter
+        );
     }
 
     pub fn test_fast_mutex() {
@@ -113,7 +123,10 @@ pub mod test {
         }
 
         // check the shared counter
-        println!("the final value of shared counter is: {:?}", **shared_counter);
+        println!(
+            "the final value of shared counter is: {:?}",
+            **shared_counter
+        );
     }
 
     pub fn test_spinlock() {
@@ -142,7 +155,10 @@ pub mod test {
         }
 
         // check the shared counter
-        println!("the final value of shared counter is: {:?}", **shared_counter);
+        println!(
+            "the final value of shared counter is: {:?}",
+            **shared_counter
+        );
     }
 
     pub fn test_resouce_lock() {
@@ -171,7 +187,10 @@ pub mod test {
         }
 
         // check the shared counter
-        println!("the final value of shared counter is: {:?}", **shared_counter);
+        println!(
+            "the final value of shared counter is: {:?}",
+            **shared_counter
+        );
     }
 
     pub fn test_queued_spin_lock() {
@@ -202,7 +221,127 @@ pub mod test {
         }
 
         // check the shared counter
-        println!("the final value of shared counter is: {:?}", **shared_counter);
+        println!(
+            "the final value of shared counter is: {:?}",
+            **shared_counter
+        );
     }
 
+    pub fn test_event() {
+        // create a auto-reset event(also called SynchronizationEvent)
+        let event = Arc::new(
+            EventProperty::new()
+                .auto_reset(true)
+                .initial_state(false)
+                .new_event()
+                .unwrap(),
+        );
+
+        // start main thread
+        {
+            let event = event.clone();
+
+            let _ = spawn(move || {
+                for i in 0..4 {
+                    this_thread::sleep(Duration::from_secs(10));
+                    event.set();
+                }
+            });
+        }
+
+        // observer thread
+        {
+            let event = event.clone();
+            let _ = spawn(move || {
+                if event.wait_for(Duration::from_secs(5), false).timed_out() {
+                    println!("wait timed out, thread {} exited", this_thread::id());
+                }
+
+                println!("observer thread {} exited", this_thread::id());
+            });
+        }
+
+        // worker thread
+        {
+            for _ in 0..4 {
+                let event = event.clone();
+                let _ = spawn(move || {
+                    if event.wait(false).success() {
+                        println!("worker thread {} waked up", this_thread::id());
+                    }
+
+                    println!("worker thread {} exited", this_thread::id());
+                });
+            }
+        }
+    }
+
+    pub fn test_semaphore() {
+        let limit = available_parallelism().get();
+
+        let semaphore = Arc::new(Semaphore::new(0, limit as _).unwrap());
+
+        // producer thread
+        {
+            let repo = semaphore.clone();
+
+            let _ = spawn(move || {
+                for _ in 0..4 {
+                    this_thread::sleep(Duration::from_secs(5));
+                    repo.release(1);
+                }
+
+                println!("producer thread {} exited", this_thread::id());
+            });
+        }
+
+        // consumer thread
+        {
+            for _ in 0..limit {
+                let repo = semaphore.clone();
+
+                let _ = spawn(move || {
+                    if repo.wait(false).success() {
+                        println!("consumerthread {} wake up", this_thread::id());
+                    }
+
+                    println!("consumer thread {} exited", this_thread::id());
+                });
+            }
+        }
+    }
+
+    pub fn test_timer() {
+        // this timer will use a DPC
+        let timer = Arc::new(
+            Timer::new(
+                || {
+                    println!("timer expired");
+                },
+                false,
+            )
+            .unwrap(),
+        );
+
+        {
+            let timer = timer.clone();
+
+            let _ = spawn(move || {
+                this_thread::sleep(Duration::from_secs(30));
+                timer.stop();
+
+                println!("timer stopped");
+            });
+        }
+
+        timer.start(Duration::ZERO, Duration::from_secs(5));
+    }
+
+    pub fn test_dpc() {
+        dpc::run_once_per_core(|| {
+            let core = unsafe { KeGetCurrentProcessorNumberEx(ptr::null_mut()) };
+
+            println!("running on core#{}", core);
+        });
+    }
 }
